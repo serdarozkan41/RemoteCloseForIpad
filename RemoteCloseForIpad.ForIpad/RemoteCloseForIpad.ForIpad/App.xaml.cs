5f38f89a068e5ccf2799457fe3a1afe0f065021c
Serdar Özkan <serdar.ozkan@cmosrealitylab.com>
2020-02-25 19:53:05 +0300

    Add project files.

---
+++ RemoteCloseForIpad.ForIpad/RemoteCloseForIpad.ForIpad/App.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Sockets.Plugin;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RemoteCloseForIpad.ForIpad
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected async override void OnStart()
+        {
+            try
+            {
+
+          
+            var address = "192.168.200.200";
+            var port = 13000;
+            var r = new Random();
+
+            var client = new TcpSocketClient();
+            await client.ConnectAsync(address, port);
+
+            // we're connected!
+            
+                var msg = "Kapat";
+                var msgBytes = Encoding.UTF8.GetBytes(msg);
+
+                await client.WriteStream.WriteAsync(msgBytes, 0, msgBytes.Length);
+
+                await client.WriteStream.FlushAsync();
+               
+           
+
+            await client.DisconnectAsync();
+            }
+            catch
+            {
+                Debug.WriteLine("Olmadı");
+            }
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RemoteCloseForIpad.ForPC/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace RemoteCloseForIpad.ForPC
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.200.200");
+                server = new TcpListener(localAddr, port);
+                server.Start();
+                Byte[] bytes = new Byte[256];
+                String data = null;
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+                    data = null;
+                    NetworkStream stream = client.GetStream();
+                    int i;
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        if (data == "Kapat")
+                        {
+                            var psi = new ProcessStartInfo("shutdown", "/s /t /f 0");
+                            psi.CreateNoWindow = true;
+                            psi.UseShellExecute = false;
+                            Process.Start(psi);
+                        }
+                    }
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
